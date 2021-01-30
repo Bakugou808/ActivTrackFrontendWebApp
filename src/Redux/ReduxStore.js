979b4d44d22a715cd14082752ece19becd1f9277
Janu <janu.m.sung@gmail.com>
2021-01-29 18:07:38 -0800

    took out the redux devtools

--- src/Redux/ReduxStore.js
+++ src/Redux/ReduxStore.js
@@ -6,7 +6,7 @@ import rootReducer from "./Reducers/RootReducer";
 export const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk, logger),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk, logger)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
